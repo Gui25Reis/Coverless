cbf47a211dfb1ec24a74404fd481c4a5f9ba3ab4
Gui Reis <gui.sreis25@gmail.com>
2021-09-14 14:51:17 -0300

    Criando projeto no XCode
    Adicionando o projeto com as pré configurações:
    - Sem Storyboard
    - Pastas separadas por tipos de arquivos
    - Templetes de cada arquivo

---
+++ Coverless/Coverless/Controllers/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Coverless
+//
+//  Created by Gui Reis on 14/09/21.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    
+    /* MARK: - Ciclo de Vida */
+    
+    public override func viewDidLoad() -> Void {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+    /* MARK: - Configurações */
+    
+    // Coloque aqui as funções de consfigurações em geral
+    
+    
+    
+    /* MARK: - Ações dos botões */
+    
+    // Coloque aqui as açòes dos botões da view
+    
+}
---
+++ Coverless/Coverless/Views/MenuView.swift
@@ -0,0 +1,40 @@
+//
+//  MenuView.swift
+//  Coverless
+//
+//  Created by Gui Reis on 14/09/21.
+//
+
+import UIKit
+
+class MenuView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+
+    }
+    
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+    
+    
+    /* MARK: - Encapsulamento */
+    
+    // Coloque aqui as funções de encapsulamentos
+    
+    
+    
+    /* MARK: - Criação */
+    
+    // Coloque aqui as funções de criações (labels, botões..)
+
+    
+    
+    /* MARK: - Constraints */
+    
+    public override func layoutSubviews() -> Void {
+        super.layoutSubviews()
+        
+        // Coloque as constraints aqui
+    }
+
+}
