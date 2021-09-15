b828d9efdbe1db8e8a7573ed52a1cd79efd03319
Gui Reis <gui.sreis25@gmail.com>
2021-09-15 14:28:09 -0300

    api no celular funcionando
    Puxa livros diretamente da API filtrando.

---
+++ Coverless/Coverless/BookRepository.swift
@@ -0,0 +1,95 @@
+//
+//  BookRepository.swift
+//  Coverless
+//
+//  Created by Gui Reis on 14/09/21.
+//
+
+import UIKit
+import Foundation
+
+
+/* MARK: - Structs para receber dados da API */
+
+struct Items: Decodable {
+    let items:[BookId]
+}
+
+struct BookId: Decodable {
+    let id:String
+    let volumeInfo:BookInformation
+}
+
+struct BookInformation: Decodable {
+    var title:String?
+    var description:String?
+    var language:String?
+}
+
+
+struct Book {
+    let id:String
+    let title:String
+    let description:String
+}
+
+
+
+class BookRepository {
+    private let token = "AIzaSyCN2r5ED0n-OeohIezXvWCGzskdhZ61x-E"
+    
+    public func getRandomizedBooks(_ completionHandler: @escaping (Result<[Book], Error>) -> Void) {
+        
+        let session = URLSession.shared
+        
+        let randomNumber:Int = Int.random(in: 0...500)
+        
+        let apiUrl = "https://www.googleapis.com/books/v1/volumes?q=aventura+subject:&startIndex=\(randomNumber)&maxResults=40"
+
+        session.dataTask(with: URL(string: apiUrl+"&key=\(self.token)")!) { data, response, error in
+            
+            if let error = error {
+                completionHandler(.failure(error))
+            } else {
+                
+                if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+                    // print("Requisição bem sucessida -> Index: \(start)")
+                    
+                    if let data = data {
+                        let decoder = JSONDecoder()
+                        let books = try! decoder.decode(Items.self, from: data)
+                        
+                        completionHandler(.success(self.compactInfo(items: books)))
+                    }
+                }
+            }
+            
+        }.resume()
+    }
+    
+    
+    private func compactInfo (items:Items) -> [Book] {
+        // Uso com Filter
+        return items.items.filter { item in
+            item.volumeInfo.title != nil &&
+            item.volumeInfo.description != nil &&
+            item.volumeInfo.language != nil &&
+            item.volumeInfo.language == "pt"
+        }.map {
+            Book(id: $0.id, title: $0.volumeInfo.title! , description: $0.volumeInfo.description!)
+        }
+        
+        // Uso com for
+        /*
+        var allBooks:[Book] = []
+        for b in items.items {
+            if (b.volumeInfo.title != nil && b.volumeInfo.description != nil) {
+                allBooks.append(
+                    Book(id: b.id, title: b.volumeInfo.title!, description: b.volumeInfo.description!)
+                )
+            }
+        }
+        return allBooks
+        */
+    }
+}
--- Coverless/Coverless/Controllers/ViewController.swift
+++ Coverless/Coverless/Controllers/ViewController.swift
@@ -9,24 +9,74 @@ import UIKit
 
 class MenuViewController: UIViewController {
     
+    private let api = BookRepository()
+    private var button:UIButton!
     
-    /* MARK: - Ciclo de Vida */
     
-    public override func viewDidLoad() -> Void {
-        super.viewDidLoad()
+    /* MARK: - Civlo de Vida */
+    public override func loadView() -> Void {
+        super.loadView()
         
+        self.view.backgroundColor = .systemGray3
+        
+        let button:UIButton = {
+            let bt = UIButton()
+            bt.backgroundColor = .systemBlue
+            bt.translatesAutoresizingMaskIntoConstraints = false
+            return bt
+        }()
+        
+        button.addTarget(self, action: #selector(self.buttonAction), for: .touchDown)
+        
+        self.button = button
+        self.view.addSubview(self.button)
+    }
+    
+    
+    public override func viewDidLayoutSubviews() -> Void {
+        super.viewDidLayoutSubviews()
+        
+        let size:CGFloat = 200
+        
+        self.button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        self.button.heightAnchor.constraint(equalToConstant: size).isActive = true
+        self.button.widthAnchor.constraint(equalToConstant: size).isActive = true
     }
     
     
     
     /* MARK: - Configurações */
+    private func success(_ books:[Book]) -> Void {
+        for b in books {
+            print("Id: \(b.id)")
+            print("Title: \(b.title)")
+            print("Sinopse: \(b.description)\n\n")
+        }
+        
+        print("===== Total de livros: \(books.count) =====\n")
+    }
     
-    // Coloque aqui as funções de consfigurações em geral
     
     
     
-    /* MARK: - Ações dos botões */
+    /* MARK: - Ação do botão */
     
-    // Coloque aqui as açòes dos botões da view
-    
+    @objc func buttonAction() -> Void {
+        
+        api.getRandomizedBooks() { result in
+            
+            switch result {
+                case .success(let book):
+                    self.success(book)
+                    break
+                    
+                case .failure(let error):
+                    print("Erro API: \(error)")
+                    break
+                    
+            }
+            
+        }
+    }
 }
